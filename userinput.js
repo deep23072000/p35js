88f1bd345e03a909a0c0098e7a85342bc35808a8
Deepak patel <dp089901@gmail.com>
2024-09-26 12:32:59 +0530

    added

---
+++ userinput.js
@@ -0,0 +1,12 @@
+function data(){
+    let name = document.querySelector('#name').value;
+    let age = document.querySelector('#age').value;
+
+    //make a registration form and show value in console
+
+    if(name === ""){
+        window.alert("Please enter your name");
+        return false;
+    }
+    
+}
